7219fdbd013680be5ada6d5203f1db781d8dba5c
Ranveer <151761152+RanveerVaru@users.noreply.github.com>
2025-11-24 21:31:25 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api/v1" : "https://chat-app-jps9.onrender.com/api/v1",
+    baseURL: "https://chat-app-jps9.onrender.com/api/v1",
     withCredentials: true,
-
 });
+
 
