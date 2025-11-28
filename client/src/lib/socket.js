dbabfd36194bbabd61f8a22a4d5553f7d84d1d43
Ranveer <151761152+RanveerVaru@users.noreply.github.com>
2025-11-28 22:48:30 +0530

    Update socket.js

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,20 +1,21 @@
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
 let socket = null;
 
-export const connectSocket = (userId)=>{
+export const connectSocket = (userId) => {
+  if (!socket) {
     socket = io("https://chat-app-jps9.onrender.com", {
-        query:{userId}
-    }) 
-    return socket;
-}
+      query: { userId },
+    });
+  }
+  return socket;
+};
 
-export const getSocket = ()=>socket;
+export const getSocket = () => socket;
 
-export const disconnectSocket = () =>{
-    if(socket){
-        socket.disconnect();
-        socket = null;
-    }
-
-}
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
